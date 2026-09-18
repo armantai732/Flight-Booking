fbe4bef4c126faf548152b3245e24ced3396268d
armantai732 <taimunna722@gmail.com>
2026-09-18 16:06:08 +0530

    cors

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,10 @@ const { default: routes } = await import("./router/router.js");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://flight-booking-vgm.vercel.app"
+    ],
     credentials: true
 }));
 
