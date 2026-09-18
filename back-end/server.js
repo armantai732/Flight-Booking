68bb5bc43a544cdfc62a32f2a7aba09b40ede8fc
armantai732 <taimunna722@gmail.com>
2026-09-18 21:24:39 +0530

    change

--- back-end/server.js
+++ back-end/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "flight-booking-hazel.vercel.app"
+        "https://flight-booking-hazel.vercel.app"
     ],
     credentials: true
 }));
